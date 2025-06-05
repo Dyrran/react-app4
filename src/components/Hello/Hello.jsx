2cece0e32e7d3f009d2732b07caa85d6893b0c29
kleiner garcia <kmgl0201@gmail.com>
2025-06-04 20:31:28 -0400

    feat: add hello.jsx , size

---
+++ src/components/Hello/Hello.jsx
@@ -0,0 +1,11 @@
+import "./Hello.css"
+export default function Hello(){
+    return (
+         <div className="bg-yellow size ">
+        <p style={{ color:"red" }}>Hola mundo.</p>
+      </div>
+
+    )
+}
+
+
